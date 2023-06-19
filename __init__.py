46e97a563cf0b4fd47c289d46f679c26d4f1a583
Rui <rui.d.figueiredo@gmail.com>
2023-06-19 22:21:10 +0100

    __init__ all

--- __init__.py
+++ __init__.py
@@ -2,4 +2,12 @@ from .message_store import MessageStore
 from .message import Message
 from .message_metadata import MessageMetadata
 from .message_from_subscription import MessageFromSubscription
-from .fetch.projection import Projection
+from .projections.projection import Projection
+
+__all__ = [
+    "MessageStore",
+    "Message",
+    "MessageMetadata",
+    "MessageFromSubscription",
+    "Projection",
+]
