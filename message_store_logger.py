581a2949832e241d2fd8fc63e0efb22f1bee52a8
Alex Cannan <alexfcannan@gmail.com>
2023-09-19 10:06:41 -0400

    revert logging changes, handle that from zenai

--- message_store_logger.py
+++ message_store_logger.py
@@ -1,8 +1,2 @@
-"""Try and use zenai logger, otherwise use logging."""
-
-try:
-    from zenai.logger import logger
-    message_store_logger = logger
-except ImportError:
-    import logging
-    message_store_logger = logging.getLogger("MessageStore")
+import logging
+message_store_logger = logging.getLogger("MessageStore")
