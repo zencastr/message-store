bd44a4d72a8d139d20eb71c8fa3565be52eb9f11
Rui Figueiredo <rui.d.figueiredo@gmail.com>
2025-01-09 14:08:36 +0000

    added assertion on caling subscribe

--- test/fetch_test.py
+++ test/fetch_test.py
@@ -9,12 +9,15 @@ class FetchTests(unittest.TestCase):
     def test_async_fetch_no_messages_returns_init(self):
         projection = mock.Mock()
         projection.get_result.return_value = {"result": "init"}
-        fetch = TestableFetch()
+        fetch = TestableFetch(nats_prefix="the_nats_env_subject_prefix.")
 
         result = asyncio.run(fetch.fetch("some_subject.123", projection))
 
-        self.assertEqual(result, {"result": "init"})
         fetch.ensure_consumer_is_deleted_mock.assert_called_once()
+        fetch.subscribe_mock.assert_called_once_with(
+            f"the_nats_env_subject_prefix.some_subject.123", ordered_consumer=True
+        )
+        self.assertEqual(result, {"result": "init"})
 
     def test_async_fetch_with_one_message_count_message_projection_returns_one(self):
         projection = Projection(
@@ -23,10 +26,14 @@ class FetchTests(unittest.TestCase):
         )
         fetch = TestableFetch(
             messages_to_return=[{"type": "TheEvent", "data": {}}],
+            nats_prefix="the_nats_env_subject_prefix.",
         )
 
         result = asyncio.run(fetch.fetch("subject", projection))
 
+        fetch.subscribe_mock.assert_called_once_with(
+            f"the_nats_env_subject_prefix.subject", ordered_consumer=True
+        )
         fetch.ensure_consumer_is_deleted_mock.assert_called_once()
         self.assertEqual(result, {"count": 1})
 
@@ -38,6 +45,7 @@ class FetchTests(unittest.TestCase):
             handlers={"TheEvent": lambda state, _: {"count": state["count"] + 1}},
         )
         fetch = TestableFetch(
+            nats_prefix="the_nats_env_subject_prefix.",
             messages_to_return=[
                 {"type": "TheEvent", "data": {}},
                 {"type": "TheEvent", "data": {}},
@@ -48,6 +56,9 @@ class FetchTests(unittest.TestCase):
 
         result = asyncio.run(fetch.fetch("some_subject.123", projection))
 
+        fetch.subscribe_mock.assert_called_once_with(
+            f"the_nats_env_subject_prefix.some_subject.123", ordered_consumer=True
+        )
         fetch.ensure_consumer_is_deleted_mock.assert_called_once()
         self.assertEqual(result, {"count": 3})
 
@@ -59,6 +70,7 @@ class FetchTests(unittest.TestCase):
             handlers={"TheEvent": lambda state, _: {"count": state["count"] + 1}},
         )
         fetch = TestableFetch(
+            nats_prefix="the_nats_env_subject_prefix.",
             messages_to_return=[
                 {"type": "TheEvent", "data": {}},  # seq 1
                 {"type": "TheEvent", "data": {}},  # seq 2
@@ -69,6 +81,9 @@ class FetchTests(unittest.TestCase):
 
         result = asyncio.run(fetch.fetch("some_subject.123", projection, until_seq=2))
 
+        fetch.subscribe_mock.assert_called_once_with(
+            f"the_nats_env_subject_prefix.some_subject.123", ordered_consumer=True
+        )
         fetch.ensure_consumer_is_deleted_mock.assert_called_once()
         self.assertEqual(result, {"count": 2})
 
@@ -80,6 +95,7 @@ class FetchTests(unittest.TestCase):
             handlers={"TheEvent": lambda state, _: {"count": state["count"] + 1}},
         )
         fetch = TestableFetch(
+            nats_prefix="the_nats_env_subject_prefix.",
             messages_to_return=[
                 {"type": "TheEvent", "data": {}},  # seq 1
                 {"type": "UnrelatedEvent", "data": {}},  # seq 2
@@ -90,30 +106,34 @@ class FetchTests(unittest.TestCase):
 
         result = asyncio.run(fetch.fetch("some_subject.123", projection, until_seq=3))
 
+        fetch.subscribe_mock.assert_called_once_with(
+            f"the_nats_env_subject_prefix.some_subject.123", ordered_consumer=True
+        )
         fetch.ensure_consumer_is_deleted_mock.assert_called_once()
         self.assertEqual(result, {"count": 2})
 
 
 class TestableFetch(Fetch):
-    def __init__(self, messages_to_return=[]):
+    def __init__(
+        self, messages_to_return=[], nats_prefix="the_nats_env_subject_prefix."
+    ):
         self.ensure_consumer_is_deleted_mock = mock.AsyncMock()
         self._ensure_consumer_is_deleted = self.ensure_consumer_is_deleted_mock
-        self.jetstrean_client_mock = mock.Mock(
-            subscribe=mock.AsyncMock(
-                return_value=mock.AsyncMock(
-                    messages=self._create_messages_iterator(
-                        subject="some_subject_value_not_used",
-                        messages=messages_to_return,
-                    ),
-                    consumer_info=mock.AsyncMock(
-                        return_value=mock.Mock(
-                            num_pending=len(messages_to_return), delivered=None
-                        )
-                    ),
-                )
+        self.subscribe_mock = mock.AsyncMock(
+            return_value=mock.AsyncMock(
+                messages=self._create_messages_iterator(
+                    subject="some_subject_value_not_used",
+                    messages=messages_to_return,
+                ),
+                consumer_info=mock.AsyncMock(
+                    return_value=mock.Mock(
+                        num_pending=len(messages_to_return), delivered=None
+                    )
+                ),
             )
         )
-        super().__init__(self.jetstrean_client_mock, "the_prefix_doesnt_matter")
+        self.jetstrean_client_mock = mock.Mock(subscribe=self.subscribe_mock)
+        super().__init__(self.jetstrean_client_mock, nats_prefix)
 
     async def _create_messages_iterator(self, subject, messages):
         for index, message in enumerate(messages):
